0f27a6bd5c2bad3286cb172e16a843e8b2256e97
Everton Barbosa Jr <evertonjrccomp@gmail.com>
2022-10-31 20:34:00 -0300

    Add implementation for the cart item related repositories

---
+++ src/Infrastructure/Persistence/Doctrine/DoctrineCreateCartItemRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Infrastructure\Persistence\Doctrine;
+
+use App\Domain\Model\CartItem;
+use App\Domain\Repository\CreateCartItemRepository;
+use Doctrine\ORM\EntityManager;
+
+class DoctrineCreateCartItemRepository implements CreateCartItemRepository
+{
+    public function __construct(
+        private readonly EntityManager $entityManager
+    ) {}
+
+    public function create(CartItem $cartItem): void
+    {
+        $this->entityManager->persist($cartItem);
+        $this->entityManager->flush();;
+    }
+}
