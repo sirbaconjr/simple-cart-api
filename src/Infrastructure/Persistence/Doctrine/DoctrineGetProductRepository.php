e905de4c084fe3e7f3490d61477f7c54fd30a164
Everton Barbosa Jr <evertonjrccomp@gmail.com>
2022-10-31 20:33:49 -0300

    Add implementation for the product related repositories

---
+++ src/Infrastructure/Persistence/Doctrine/DoctrineGetProductRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Infrastructure\Persistence\Doctrine;
+
+use App\Domain\Exception\ProductNotFound;
+use App\Domain\Model\Product;
+use App\Domain\Repository\GetProductRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Uid\UuidV4;
+
+class DoctrineGetProductRepository implements GetProductRepository
+{
+    public function __construct(
+        private readonly EntityManager $entityManager
+    ) {}
+
+    public function getProduct(UuidV4 $id): Product
+    {
+        $product = $this->entityManager
+            ->getRepository(Product::class)
+            ->find($id);
+
+        if ($product == null) {
+            throw new ProductNotFound($id);
+        }
+
+        return $product;
+    }
+}
